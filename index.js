1220892287461c02c38e6de1667d53b956920499
Mike Curtis <chesterfied@chesterfied.com>
2018-08-01 15:44:43 -0700

    add uuids to clients.  return appropriate status objects via json

--- index.js
+++ index.js
@@ -6,27 +6,32 @@ const API_PREFIX = '/ifttt/v1';
 const ACTIONS_PREFIX = API_PREFIX + '/actions'
 const ACTIONS_PATTERN = ACTIONS_PREFIX + '/:action'
 
+function writeReply(res, sts, obj) {
+	res.status(sts);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(obj));
+}
+
 const server = express()
 	.use(express.static('static'))
 	.use('/', express.static('/static/index.html'))
 	.post(ACTIONS_PATTERN, function (req, res) {
 		var action = req.params.action
-		console.log(action)
 		wss.clients.forEach((client) => {
-			console.log('forward ' + action + ' to client ' + client);
+			console.log('Forward command "' + action + '" to client: ' + client.id);
 			client.send(action);
 		});
+		writeReply(res, 200, {'data': {}});
 		// TODO: propagate errors back and handle error case
-		// res.writeHead(500);
-		// return res.end('Error loading ' + path);
-		res.writeHead(200);
-		res.end('acknowledged: "' + action + '"');
+		// writeReply(res, 500, {'errors': [{'message': 'Blah!'}] });
 	})
 	.listen(PORT, () => console.log('Listening on ' + PORT));
 
 const SocketServer = require('ws').Server;
 const wss = new SocketServer({ server });
+const uuid = require('uuid');
 wss.on('connection', (ws) => {
-	console.log('Client connected');
-	ws.on('close', () => console.log('Client disconnected'));
+	ws.id = uuid.v4();
+	console.log('Client connected: ' + ws.id);
+	ws.on('close', () => console.log('Client disconnected: ' + ws.id));
 });
